96d7875abe0881fc8d0dfbeaa37b112b37cc6b53
gmdayley <gabriel.dayley@gmail.com>
2013-06-15 22:33:12 -0600

    skeleton

---
+++ app.js
@@ -0,0 +1,55 @@
+var  express = require('express')
+    , app = express()
+    , server = require('http').createServer(app)
+    , twilio = require('twilio');
+
+// Load twilio properties
+var TWILIO_SID = process.env.TWILIO_SID,
+	TWILIO_AUTHTOKEN = process.env.TWILIO_AUTHTOKEN;
+
+// Create twilio client
+var twilioClient = twilio(TWILIO_SID, TWILIO_AUTHTOKEN);
+
+
+// Start server
+var port = process.env.PORT || 5000
+server.listen(port);
+console.log("server listening on port " + port);
+
+
+// Configure express
+app.use(express.bodyParser());
+app.use("/", express.static(__dirname + '/public'));
+
+
+// Configure routes
+app.get('/twilio/account', function(request, response) {
+	twilioClient.accounts(TWILIO_SID).get(function(err, account) {
+    	if(err) {
+    		response.send({});
+    	} else {
+    		response.send({
+    			friendlyName: account.friendly_name,
+    			created: account.date_created,
+    			sid: account.sid,
+    			status: account.status
+    		});
+    	}
+	});
+});
+
+app.post('/sms/send/:to', function(request, response){
+	var to = request.params.to;
+	var msg = request.body;
+
+	
+	twilioClient.sendSms({
+		to: to,
+		from: msg.from,
+		body: msg.body
+	},function(error, responseData){
+		console.log('error', error, responseData);
+	});
+
+	response.end('success');
+});
